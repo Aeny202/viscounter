d087b7ec3c409cea0a4fad8e9f8ef6893c551c97
Aeny202 <aeny202+github@gmail.com>
2017-05-22 14:58:29 +0200

    -DatePicker for starting date is now set to current day -DatePicker for ending date is now set to current day + 1 -Added textview to show remaining time between the picked dates

--- app/src/main/java/com/viscount/mony/viscounter/MainActivity.java
+++ app/src/main/java/com/viscount/mony/viscounter/MainActivity.java
@@ -3,7 +3,10 @@ package com.viscount.mony.viscounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.Calendar;
 
@@ -13,28 +16,83 @@ public class MainActivity extends AppCompatActivity {
 
     private String strStartDate;
     private String strEndDate;
+    private int intStartYear;
+    private int intStartMonth;
+    private int intStartDay;
+    private int intEndYear;
+    private int intEndMonth;
+    private int intEndDay;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get the current date so we can set it as starting date for the date pickers
+        Calendar c = Calendar.getInstance();
+        ;
+
+        // Setup initial values and listener for the starting date picker
         DatePicker dpStartDate = (DatePicker)findViewById(R.id.datePicker1);
+        dpStartDate.init(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener()
+        {
+
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                intStartYear = view.getYear();
+                intStartMonth = view.getMonth();
+                intStartDay = view.getDayOfMonth();
+
+                Toast toast = Toast.makeText(MainActivity.this, intStartDay + "-"+ intStartMonth + "-" + intStartYear, Toast.LENGTH_SHORT);
+                toast.show();
+
+                setTimeRemaining(intStartYear, intStartMonth, intStartDay, intEndYear, intEndMonth, intEndDay);
+            }
+        });
+
+        // prepopulate the variables before the user triggers the onDateChanged listeners.
+        intStartYear = dpStartDate.getYear();
+        intStartMonth = dpStartDate.getMonth();
+        intStartDay = dpStartDate.getDayOfMonth();
+
+        // Setup initial values and listener for the ending date picker
         DatePicker dpEndDate = (DatePicker)findViewById(R.id.datePicker2);
-        dpStartDate.init(0, 0, 0, new DatePicker.OnDateChangedListener()
+        dpEndDate.init(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)+1, new DatePicker.OnDateChangedListener()
         {
             @Override
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth)
             {
-                Toast toast = Toast.makeText(MainActivity.this, view.getDayOfMonth() + "-"+ view.getMonth() + "-" + view.getYear(), Toast.LENGTH_LONG);
+                intEndYear = view.getYear();
+                intEndMonth = view.getMonth();
+                intEndDay = view.getDayOfMonth();
+
+                Toast toast = Toast.makeText(MainActivity.this, intStartDay + "-"+ intStartMonth + "-" + intStartYear, Toast.LENGTH_SHORT);
                 toast.show();
+
+                setTimeRemaining(intStartYear, intStartMonth, intStartDay, intEndYear, intEndMonth, intEndDay);
             }
         });
+
+        // prepopulate the variables before the user triggers the onDateChanged listeners.
+        intEndYear = dpEndDate.getYear();
+        intEndMonth = dpEndDate.getMonth();
+        intEndDay = dpEndDate.getDayOfMonth();
+
+        // Runs this once at onCreate so the field is populated when the app is started (should show 1 day remaning)
+        setTimeRemaining(intStartYear, intStartMonth, intStartDay, intEndYear, intEndMonth, intEndDay);
     }
 
-
-    public void OnDateChanged()
+    // This method calculates the difference between the start and end dates selected, then displays it in a TextView on screen.
+    private void setTimeRemaining(int intStartYear, int intStartMonth, int intStartDay, int intEndYear, int intEndMonth, int intEndDay)
     {
 
+        TextView txtTimeRemaining = (TextView) findViewById(R.id.timeRemaining);
+        int intRemainingYears = intEndYear - intStartYear;
+        int intRemaningMonths = intEndMonth - intStartMonth;
+        int intRemainingDays = intEndDay - intStartDay;
+
+        txtTimeRemaining.setText("There are "+intRemainingYears+" year(s), "+intRemaningMonths+" month(s) and "+intRemainingDays+" day(s) remaining!");
     }
+
 }
